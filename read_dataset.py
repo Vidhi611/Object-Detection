b42cd167f2623d823e617dc259bd08ca38c26192
Sony <bachina.sony@gmail.com>
2019-03-15 02:16:00 +0530

    Updated dataset preprocessing

--- read_dataset.py
+++ read_dataset.py
@@ -1,97 +1,94 @@
-#Test in draft.py
+'''Reading pascal dataset
+Every image has width, height and depth
+Each image has diff no of objects
+Each object has name, bndbox(xmin,xmax,ymin,ymax)
 
-import numpy as np
-from math import sqrt
+reading annotations'''
+
+import xml.etree.ElementTree as ET 
 import os
-import random
 import pickle
-import codecs
-'''
-calculate temporal intersection over union
-'''
-def calculate_IoU(i0, i1):
-	union = (min(i0[0], i1[0]), max(i0[1], i1[1]))
-	inter = (max(i0[0], i1[0]), min(i0[1], i1[1]))
-	iou = 1.0*(inter[1]-inter[0])/(union[1]-union[0])
-	return iou
+from PIL import Image
 
-'''
-calculate the non Intersection part over Length ratia, make sure the input IoU is larger than 0
-'''
-def calculate_nIoL(base, sliding_clip):
-	inter = (max(base[0], sliding_clip[0]), min(base[1], sliding_clip[1]))
-	inter_l = inter[1]-inter[0]
-	length = sliding_clip[1]-sliding_clip[0]
-	nIoL = 1.0*(length-inter_l)/length
-	return nIoL
+class PascalData():
+	def __init__(self, image_path, annotation_path,newshape):
+		#self.classes=classes_list
+		self.images=[]
+		self.image_files=[]
+		self.annotations={}
+		self.annotation_path=annotation_path
+		self.imagesdir_path=image_path
+		self.class_labels={'motorbike':0,'bottle':1,'bird':2,'cat':3,'aeroplane':4,'chair':5,'person':6,'diningtable':7,'boat':8,'train':9,'sofa':10,'bicycle':11,'bus':12,'horse':13,'tvmonitor':14,'cow':15,'pottedplant':16,'car':17,'dog':18,'sheep':19}
+		self.newshape=newshape
+		
+		
+	def readXmlfile(self):
+		annotations_files = os.listdir(self.annotation_path)
+		for file in annotations_files:
+			#file_details={}
+			#file=annotations_files[0]
+			file_to_parse=self.annotation_path+file
+			tree=ET.parse(file_to_parse)
+			root=tree.getroot()
+			img_filename=root.find('filename')
+			#img_filename includes .jpg also
+			#file_details.append(img_filename)
+			self.image_files.append(img_filename.text)
 
-class TrainingDataSet(object):
-
-	def __init__(self, sliding_dir, it_path, batch_size):
-		self.counter = 0
-		self.batch_size = batch_size
-		self.context_num = 1
-		self.context_size = 128
-		print("Reading training data list from "+it_path)
-		cs = pickle.load(codecs.open(it_path,"rb",encoding="iso-8859-1"))
-		#movie_length_info = pickle.load(codecs.open("./TACoS/video_allframes_info.pkl","rb",encoding="iso-8859-1"))
-		self.clip_sentence_pairs = []
-		for l in cs:
-		    clip_name = l[0]
-		    sent_vecs = l[1]
-		    for sent_vec in sent_vecs:
-		        self.clip_sentence_pairs.append((clip_name, sent_vec))
-
-		movie_names_set = set()
-		self.movie_clip_names = {}
-		# read groundtruth sentence-clip pairs
-		for k in range(len(self.clip_sentence_pairs)):
-		    clip_name = self.clip_sentence_pairs[k][0]
-		    movie_name = clip_name.split("_")[0]
-		    if not movie_name in movie_names_set:
-		        movie_names_set.add(movie_name)
-		        self.movie_clip_names[movie_name] = []
-		    self.movie_clip_names[movie_name].append(k)
-		self.movie_names = list(movie_names_set)
-		self.visual_feature_dim = 4096*3
-		self.sent_vec_dim = 4800
-		self.num_samples = len(self.clip_sentence_pairs)
-		self.sliding_clip_path = sliding_dir
-		print(str(len(self.clip_sentence_pairs))+" clip-sentence pairs are readed")
-
-		# read sliding windows, and match them with the groundtruths to make training samples
-		sliding_clips_tmp = os.listdir(self.sliding_clip_path)
-		self.clip_sentence_pairs_iou = []
-		for clip_name in sliding_clips_tmp:
-			if clip_name.split(".")[2]=="npy":
-				movie_name = clip_name.split("_")[0]
-				for clip_sentence in self.clip_sentence_pairs:
-					original_clip_name = clip_sentence[0] 
-					original_movie_name = original_clip_name.split("_")[0]
-					if original_movie_name==movie_name:
-						start = int(clip_name.split("_")[1])
-						end = int(clip_name.split("_")[2].split(".")[0])
-						o_start = int(original_clip_name.split("_")[1]) 
-						o_end = int(original_clip_name.split("_")[2].split(".")[0])
-						iou = calculate_IoU((start, end), (o_start, o_end))
-						if iou>0.5:
-							nIoL=calculate_nIoL((o_start, o_end), (start, end))
-							if nIoL<0.15:
-								#movie_length = movie_length_info[movie_name.split(".")[0]]
-								start_offset =o_start-start
-								end_offset = o_end-end
-								self.clip_sentence_pairs_iou.append((clip_sentence[0], clip_sentence[1], clip_name, start_offset, end_offset))
-		self.num_samples_iou = len(self.clip_sentence_pairs_iou)
-		#with open("TACoS_input.txt", "w") as text_file:
-			#text_file.write("Purchase Amount: %s" % TotalAmount)
-		print(str(len(self.clip_sentence_pairs_iou))+" iou clip-sentence pairs are readed")
-
-train_csv_path = "./TACoS/train_clip-sentvec.pkl"
-#test_csv_path = "./TACoS/test_clip-sentvec.pkl"
-#test_feature_dir="./TACOS/Interval128_256_overlap0.8_c3d_fc6/"
-train_feature_dir = "./TACOS/Interval64_128_256_512_overlap0.8_c3d_fc6/"
-batch_size=56
-TrainingDataSet(train_feature_dir,train_csv_path,batch_size)
+			Objects=[]
+			for element in root.findall('object'):
+				Object=[]
+				Object.append(element.find('name').text)
+				Object.append(element.find('bndbox').find('xmin').text)
+				Object.append(element.find('bndbox').find('ymin').text) 
+				Object.append(element.find('bndbox').find('xmax').text)
+				Object.append(element.find('bndbox').find('ymax').text)
+				Objects.append(Object)
+			#print(len(Objects))
+			self.annotations[img_filename.text]=Objects
+		#print(len(self.annotations))
+		#pickle.dump(self.annotations, open("./PASCAL-VOC/VOC2012/Annotations.pkl", 'wb'))
 
 
+	def buildDataset(self):
+		#X(image), Y(class) from dataset
+		self.readXmlfile()
+		X=[]
+		Y=[]
+		for image_file,annotations in self.annotations.items():
+			#append img to X
+			#append img details array to Y in the form[class,xmin,xmax,ymin,ymax]
+			#image_file=list(self.annotations.keys())[0]
+			#annotations=self.annotations[image_file]
+			filename=image_file.split(".")[0]
+			img_path=self.imagesdir_path + image_file
+			im = Image.open(img_path)
+			nx, ny = im.size
+			im2 = im.resize(self.newshape , Image.BICUBIC)
+			im2.load()
+			#print(im2.size)
+			img_array = np.asarray( im2, dtype="int32" )
+			#np.save(numpyfile_path + filename , x)
+			#print(img_array)
+			X.append(img_array)
+			y=[]
+			for annot in annotations:
+				temp=[]
+				class_label=self.class_labels[annot[0]]
+				temp.append(class_label)
+				temp.append(float(annot[1]))
+				temp.append(float(annot[2]))
+				temp.append(float(annot[3]))
+				temp.append(float(annot[4]))
+				y.append(temp)
+				#print(y)
+			Y.append(y)
+			#print(Y)
+		pickle.dump(X, open("./PASCAL-VOC/VOC2012/ProcessedDataImage.pkl", 'wb'))
+		pickle.dump(Y, open("./PASCAL-VOC/VOC2012/ProcessedDataClasses.pkl", 'wb'))
+
+
+
+ps=PascalData("./PASCAL-VOC/VOC2012/JPEGImages/","./PASCAL-VOC/VOC2012/Annotations/",(300,300))
+ps.buildDataset()
 
